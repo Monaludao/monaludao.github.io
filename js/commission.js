ea23ca962a33682e59ae02e29accd5b26f37d1e0
Richard Hsieh <monaludao@gmail.com>
2016-04-20 14:09:34 +0800

    testing js code

--- js/commission.js
+++ js/commission.js
@@ -16,6 +16,10 @@ function getData(){
 		$('.record-content').append('<p><div>'+records.report_item[0].case+'</div></p>');
 		$('.record-content').append('<p><div>說明：</div></p>');
 		$('.record-content').append('<p><div>'+records.report_item[0].description+'</div></p>');
+		$('.record-content').append('<p><div>委員發言摘要：</div></p>');
+		$('.record-content').append('<p><div>'+records.report_item[0].committee_speak+'</div></p>');
+		$('.record-content').append('<p><div>都發局回覆：</div></p>');
+		$('.record-content').append('<p><div>'+records.report_item[0].response+'</div></p>');
 		$('.record-content').append('<p><div>決議：</div></p>');
 		$('.record-content').append('<p><div>'+records.report_item[0].resolution+'</div></p>');
 
@@ -42,5 +46,24 @@ function getData(){
 		$('.record-content').append('<p><div>'+records.discuss_item[0].response+'</div></p>');
 		$('.record-content').append('<p><div>決議：</div></p>');
 		$('.record-content').append('<p><div>'+records.discuss_item[0].resolution+'</div></p>');
+
+		case_render(records.discuss_item);
 	});
 };
+
+function case_render(item_object){
+	var length = item_object.length
+	for (var i = 0; i < length; i++){
+		var con_case = item_object[i];
+		$('.record-content').append('<p><div>案名：</div></p>');
+		$('.record-content').append('<p><div>'+con_case.case+'</div></p>');
+		$('.record-content').append('<p><div>說明：</div></p>');
+		$('.record-content').append('<p><div>'+con_case.description+'</div></p>');
+		$('.record-content').append('<p><div>委員發言摘要：</div></p>');
+		$('.record-content').append('<p><div>'+con_case.committee_speak+'</div></p>');
+		$('.record-content').append('<p><div>都發局回覆：</div></p>');
+		$('.record-content').append('<p><div>'+con_case.response+'</div></p>');
+		$('.record-content').append('<p><div>決議：</div></p>');
+		$('.record-content').append('<p><div>'+con_case.resolution+'</div></p>');
+	}
+}
