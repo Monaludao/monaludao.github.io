63a2784943ddbee463eadfcc8582a544406b39a8
Richard Hsieh <monaludao@gmail.com>
2016-04-20 12:17:25 +0800

    building commission.html

--- js/commission.js
+++ js/commission.js
@@ -2,18 +2,24 @@ function getData(){
 	$.getJSON('https://raw.githubusercontent.com/g0v/urbancode-commission/master/record/TPEUP/JSON/674.json', function(data) {
 		records = data;
 		$('.record-content').append('<p><div>'+records.title+'</div></p>');
-		$('.record-content').append('<p><div>'+records.session+'</div></p>');
-		$('.record-content').append('<p><div>'+records.date+'</div></p>');
-		$('.record-content').append('<p><div>'+records.start_time+'</div></p>');
-		$('.record-content').append('<p><div>'+records.end_time+'</div></p>');
-		$('.record-content').append('<p><div>'+records.location+'</div></p>');
-		$('.record-content').append('<p><div>'+records.chairman+'</div></p>');
-		$('.record-content').append('<p><div>'+records.note_taker+'</div></p>');
-		$('.record-content').append('<p><div>'+records.attend_committee+'</div></p>');
-		$('.record-content').append('<p><div>'+records.attend_unit+'</div></p>');
-		$('.record-content').append('<p><div>'+records.report_item+'</div></p>');
+		// $('.record-content').append('<p><div>'+records.session+'</div></p>');
+		$('.record-content').append('<p><div>日期：'+records.date+'</div></p>');
+		$('.record-content').append('<p><div>時間：'+records.start_time+'-'+records.end_time+'</div></p>');
+		$('.record-content').append('<p><div>地點：'+records.location+'</div></p>');
+		$('.record-content').append('<p><div>主席：'+records.chairman+'</div></p>');
+		$('.record-content').append('<p><div>紀錄彙整：'+records.note_taker+'</div></p>');
+		$('.record-content').append('<p><div>出席委員：'+records.attend_committee+'</div></p>');
+		$('.record-content').append('<p><div>出席單位：'+records.attend_unit+'</div></p>');
+
+		$('.record-content').append('<p><div>報告事項：</div></p>');
+		$('.record-content').append('<p><div>'+records.report_item[0].case+'</div></p>');
+		$('.record-content').append('<p><div>'+records.report_item[0].description+'</div></p>');
+		$('.record-content').append('<p><div>'+records.report_item[0].resolution+'</div></p>');
+
+		$('.record-content').append('<p><div>審議事項：</div></p>');
 		$('.record-content').append('<p><div>'+records.deliberate_item+'</div></p>');
+
+		$('.record-content').append('<p><div>討論事項：</div></p>');
 		$('.record-content').append('<p><div>'+records.discuss_item+'</div></p>');
 	});
-
 };
