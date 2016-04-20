8c2e743ffbf999347568988daf8f0a7f43e47f36
Richard Hsieh <monaludao@gmail.com>
2016-04-20 14:10:54 +0800

    testing js renderor

--- js/commission.js
+++ js/commission.js
@@ -12,42 +12,45 @@ function getData(){
 		$('.record-content').append('<p><div>出席單位：'+records.attend_unit+'</div></p>');
 
 		$('.record-content').append('<p><div>報告事項：</div></p>');
-		$('.record-content').append('<p><div>案名：</div></p>');
-		$('.record-content').append('<p><div>'+records.report_item[0].case+'</div></p>');
-		$('.record-content').append('<p><div>說明：</div></p>');
-		$('.record-content').append('<p><div>'+records.report_item[0].description+'</div></p>');
-		$('.record-content').append('<p><div>委員發言摘要：</div></p>');
-		$('.record-content').append('<p><div>'+records.report_item[0].committee_speak+'</div></p>');
-		$('.record-content').append('<p><div>都發局回覆：</div></p>');
-		$('.record-content').append('<p><div>'+records.report_item[0].response+'</div></p>');
-		$('.record-content').append('<p><div>決議：</div></p>');
-		$('.record-content').append('<p><div>'+records.report_item[0].resolution+'</div></p>');
+		case_render(records.report_item);
+		// $('.record-content').append('<p><div>案名：</div></p>');
+		// $('.record-content').append('<p><div>'+records.report_item[0].case+'</div></p>');
+		// $('.record-content').append('<p><div>說明：</div></p>');
+		// $('.record-content').append('<p><div>'+records.report_item[0].description+'</div></p>');
+		// $('.record-content').append('<p><div>委員發言摘要：</div></p>');
+		// $('.record-content').append('<p><div>'+records.report_item[0].committee_speak+'</div></p>');
+		// $('.record-content').append('<p><div>都發局回覆：</div></p>');
+		// $('.record-content').append('<p><div>'+records.report_item[0].response+'</div></p>');
+		// $('.record-content').append('<p><div>決議：</div></p>');
+		// $('.record-content').append('<p><div>'+records.report_item[0].resolution+'</div></p>');
 
 		$('.record-content').append('<p><div>審議事項：</div></p>');
-		$('.record-content').append('<p><div>案名：</div></p>');
-		$('.record-content').append('<p><div>'+records.deliberate_item[0].case+'</div></p>');
-		$('.record-content').append('<p><div>說明：</div></p>');
-		$('.record-content').append('<p><div>'+records.deliberate_item[0].description+'</div></p>');
-		$('.record-content').append('<p><div>委員發言摘要：</div></p>');
-		$('.record-content').append('<p><div>'+records.deliberate_item[0].committee_speak+'</div></p>');
-		$('.record-content').append('<p><div>都發局回覆：</div></p>');
-		$('.record-content').append('<p><div>'+records.deliberate_item[0].response+'</div></p>');
-		$('.record-content').append('<p><div>決議：</div></p>');
-		$('.record-content').append('<p><div>'+records.deliberate_item[0].resolution+'</div></p>');
+		case_render(records.deliberate_item);
+		// $('.record-content').append('<p><div>案名：</div></p>');
+		// $('.record-content').append('<p><div>'+records.deliberate_item[0].case+'</div></p>');
+		// $('.record-content').append('<p><div>說明：</div></p>');
+		// $('.record-content').append('<p><div>'+records.deliberate_item[0].description+'</div></p>');
+		// $('.record-content').append('<p><div>委員發言摘要：</div></p>');
+		// $('.record-content').append('<p><div>'+records.deliberate_item[0].committee_speak+'</div></p>');
+		// $('.record-content').append('<p><div>都發局回覆：</div></p>');
+		// $('.record-content').append('<p><div>'+records.deliberate_item[0].response+'</div></p>');
+		// $('.record-content').append('<p><div>決議：</div></p>');
+		// $('.record-content').append('<p><div>'+records.deliberate_item[0].resolution+'</div></p>');
 
 		$('.record-content').append('<p><div>討論事項：</div></p>');
-		$('.record-content').append('<p><div>案名：</div></p>');
-		$('.record-content').append('<p><div>'+records.discuss_item[0].case+'</div></p>');
-		$('.record-content').append('<p><div>說明：</div></p>');
-		$('.record-content').append('<p><div>'+records.discuss_item[0].description+'</div></p>');
-		$('.record-content').append('<p><div>委員發言摘要：</div></p>');
-		$('.record-content').append('<p><div>'+records.discuss_item[0].committee_speak+'</div></p>');
-		$('.record-content').append('<p><div>都發局回覆：</div></p>');
-		$('.record-content').append('<p><div>'+records.discuss_item[0].response+'</div></p>');
-		$('.record-content').append('<p><div>決議：</div></p>');
-		$('.record-content').append('<p><div>'+records.discuss_item[0].resolution+'</div></p>');
-
 		case_render(records.discuss_item);
+		// $('.record-content').append('<p><div>案名：</div></p>');
+		// $('.record-content').append('<p><div>'+records.discuss_item[0].case+'</div></p>');
+		// $('.record-content').append('<p><div>說明：</div></p>');
+		// $('.record-content').append('<p><div>'+records.discuss_item[0].description+'</div></p>');
+		// $('.record-content').append('<p><div>委員發言摘要：</div></p>');
+		// $('.record-content').append('<p><div>'+records.discuss_item[0].committee_speak+'</div></p>');
+		// $('.record-content').append('<p><div>都發局回覆：</div></p>');
+		// $('.record-content').append('<p><div>'+records.discuss_item[0].response+'</div></p>');
+		// $('.record-content').append('<p><div>決議：</div></p>');
+		// $('.record-content').append('<p><div>'+records.discuss_item[0].resolution+'</div></p>');
+
+
 	});
 };
 
