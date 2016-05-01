271eae526e6e90557952f564ec2246982e01dfc5
Richard Hsieh <monaludao@gmail.com>
2016-05-01 12:51:47 +0800

    rewrite html structure

--- js/commission.js
+++ js/commission.js
@@ -4,103 +4,109 @@ function getData(){
 
 		$('.record-content').append('<div class="note-title"><p><span>'+records.title+'</span></p></div>');
 
-		var $notehead = $('<div class="note-head"/>');
-
-		$($notehead).append('<p><span>日期：</span><span>'+records.date+'</span></p>');
-		$($notehead).append('<p><span>日期：</span><span>'+records.date+'</span></p>');
-		$($notehead).append('<p><span>時間：</span><span>'+records.start_time+'-'+records.end_time+'</span></p>');
-		$($notehead).append('<p><span>地點：</span><span>'+records.location+'</span></p>');
-		$($notehead).append('<p><span>主席：</span><span>'+records.chairman+'</span></p>');
-		$($notehead).append('<p><span>紀錄彙整：</span><span>'+records.note_taker+'</span></p>');
-		$($notehead).append('<p><span>出席委員：</span><span>'+records.attend_committee+'</span></p>');
-		$($notehead).append('<p><span>出席單位：</span><span>'+records.attend_unit+'</span></p>');
-		$('.record-content').append($notehead);
+		var $note_head = $('<div class="note-head"/>');
+		$($note_head).append('<p><span class="heading">日期：</span><span>'+records.date+'</span></p>');
+		$($note_head).append('<p><span class="heading">時間：</span><span>'+records.start_time+'-'+records.end_time+'</span></p>');
+		$($note_head).append('<p><span class="heading">地點：</span><span>'+records.location+'</span></p>');
+		$($note_head).append('<p><span class="heading">主席：</span><span>'+records.chairman+'</span></p>');
+		$($note_head).append('<p><span class="heading">紀錄彙整：</span><span>'+records.note_taker+'</span></p>');
+		$($note_head).append('<p><span class="heading">出席委員：</span><span>'+records.attend_committee+'</span></p>');
+		$($note_head).append('<p><span class="heading">出席單位：</span><span>'+records.attend_unit+'</span></p>');
+		$('.record-content').append($note_head);
 
 		if(records.report_item != null){
-			$('.record-content').append('<div class="report item">');
-			$('.record-content').append('<p>報告事項：</p>');
-			item_render(records.report_item);
-			$('.record-content').append('</div>');
+			var $report_item = $('<div class="report item"/>');
+			$($report_item).append('<p><span class="heading">報告事項：</span></p>');
+			$($report_item).append(item_render(records.report_item));
+			$('.record-content').append($report_item);
 		};
 		if(records.deliberate_item != null){
-			$('.record-content').append('<div class="deliberate item">');
-			$('.record-content').append('<div><p>審議事項：</p></div>');
-			item_render(records.deliberate_item);
-			$('.record-content').append('</div>');
+			var $deliberate_item = $('<div class="deliberate item"/>');
+			$($deliberate_item).append('<p><span class="heading">審議事項：</span></p>');
+			$($deliberate_item).append(item_render(records.deliberate_item));
+			$('.record-content').append($deliberate_item);
 		};
 		if(records.discuss_item != null){
-			$('.record-content').append('<div class="discuss item">');
-			$('.record-content').append('<div><p>討論事項：</p></div>');
-			item_render(records.discuss_item);
-			$('.record-content').append('</div>');
+			var $discuss_item = $('<div class="discuss item"/>');
+			$($discuss_item).append('<p><span>討論事項：</span></p>');
+			$($discuss_item).append(item_render(records.discuss_item));
+			$('.record-content').append($discuss_item);
 		};
 	});
 };
 
 function item_render(item_object){
-	var length = item_object.length
+	var length = item_object.length;
+	var $item_html = $('<div class="item content"/>');
 	for (var i = 0; i < length; i++){
 		var con_case = item_object[i];
-		$('.record-content').append('<div><p>案名：</p></div>');
-		$('.record-content').append('<div><p>'+con_case.case+'</p></div>');
+		$($item_html).append('<p><span class="heading">案名：</span><span>'+con_case.case+'</span></p>');
+
 		if(con_case.description != null){
-			$('.record-content').append('<div><p>說明：</p></div>');
-			array_render(con_case.description);
+			$($item_html).append('<p><span class="heading">說明：</span></p>');
+			$($item_html).append(array_render(con_case.description));
 		};
 		if(con_case.committee_speak != null){
-			$('.record-content').append('<div><p>委員發言摘要：</p></div>');
-			speak_render(con_case.committee_speak);
+			$($item_html).append('<p><span class="heading">委員發言摘要：</span></p>');
+			$($item_html).append(speak_render(con_case.committee_speak));
 		};
 		if(con_case.response != null){
-			$('.record-content').append('<div><p>都發局回覆：</p></div>');
-			array_render(con_case.response);
+			$($item_html).append('<p><span class="heading">都發局回覆：</span></p>');
+			$($item_html).append(array_render(con_case.response));
 		};
 		if(con_case.resolution != null){
-			$('.record-content').append('<div><p>決議：</p></div>');
-			array_render(con_case.resolution);
+			$($item_html).append('<p><span class="heading">決議：</span></p>');
+			$($item_html).append(array_render(con_case.resolution));
 		};
 		if(con_case.petition != null){
-			$('.record-content').append('<div><p>人民陳情意見：</p></div>');
-			petition_render(con_case.petition);
+			$($item_html).append('<p><span class="heading">人民陳情意見：</span></p>');
+			$($item_html).append(petition_render(con_case.petition));
 		};
 	}
+	return $item_html;
 }
 
 function array_render(con_array){
-	var length = con_array.length
+	var length = con_array.length;
+	var $array_html = $('<div class="content list"/>');
 	for(var i = 0; i < length; i++){
-		$('.record-content').append('<div><p>'+con_array[i]+'</p></div>');
+		$($array_html).append('<p><span>'+con_array[i]+'</span></p>');
 	}
+	return $array_html;
 }
 
 function speak_render(con_speak){
 	var length = con_speak.length
+	var $speak_html = $('<div class="content list"/>');
 	for(var i = 0; i < length; i++){
-		$('.record-content').append('<div><p>'+con_speak[i].committee+'</p></div>');
-		$('.record-content').append('<div><p>'+con_speak[i].speak+'</p></div>');
+		$($speak_html).append('<p><span>'+con_speak[i].committee+'</span></p>');
+		$($speak_html).append('<p><span>'+con_speak[i].speak+'</span></p>');
 	}
+	return $speak_html;
 }
 
 function petition_render(con_petition){
 	var length = con_petition.length
+	var $petition_html = $('<div calss="content list"/>')
 	for(var i = 0; i < length; i++){
-		$('.record-content').append('<div><p>編號：'+con_petition[i].pet_num+'</p></div>');
-		$('.record-content').append('<div><p>陳情人：'+con_petition[i].pet_name+'</p></div>');
+		$($petition_html).append('<p><span class="heading">編號：</span><span>'+con_petition[i].pet_num+'</span></p>');
+		$($petition_html).append('<p><span class="heading">陳情人：</span><span>'+con_petition[i].pet_name+'</span></p>');
 		if(con_petition[i].pet_reason != null){
-			$('.record-content').append('<div><p>陳情理由：</p></div>');
-			array_render(con_petition[i].pet_reason);
+			$($petition_html).append('<p><span class="heading">陳情理由：</span></p>');
+			$($petition_html).append(array_render(con_petition[i].pet_reason));
 		};
 		if(con_petition[i].pet_suggest != null){
-			$('.record-content').append('<div><p>建議辦法：</p></div>');
-			array_render(con_petition[i].pet_suggest);
+			$($petition_html).append('<p><span class="heading">建議辦法：</span></p>');
+			$($petition_html).append(array_render(con_petition[i].pet_suggest));
 		};
 		if(con_petition[i].pet_response != null){
-			$('.record-content').append('<div><p>主管單位回應：</p></div>');
-			array_render(con_petition[i].pet_response);
+			$($petition_html).append('<p><span class="heading">主管單位回應：</span></p>');
+			$($petition_html).append(array_render(con_petition[i].pet_response));
 		};
 		if(con_petition[i].pet_resolution != null){
-			$('.record-content').append('<div><p>委員會決議：</p></div>');
-			array_render(con_petition[i].pet_resolution);
+			$($petition_html).append('<p><span class="heading">委員會決議：</span></p>');
+			$($petition_html).append(array_render(con_petition[i].pet_resolution));
 		}
 	}
+	return $petition_html;
 }
